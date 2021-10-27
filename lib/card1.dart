6b53d107d045121cd30881004a00a6a5110b8c3a
webmategh@gmail.com <webmategh@gmail.com>
2021-10-27 12:03:54 +0100

    complete 3

--- lib/card1.dart
+++ lib/card1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
 
 class Card1 extends StatelessWidget {
   const Card1({Key? key}) : super(key: key);
@@ -18,6 +19,42 @@ class Card1 extends StatelessWidget {
       child: Container(
         // ignore: todo
         // TODO: Add a stack of text
+        child: Stack(
+          children: [
+            // 8
+            Text(
+              category,
+              style: FooderlichTheme.darkTextTheme.bodyText1,
+            ),
+            // 9
+            Positioned(
+              child: Text(
+                title,
+                style: FooderlichTheme.darkTextTheme.headline2,
+              ),
+              top: 20,
+            ),
+            // 10
+            Positioned(
+              child: Text(
+                description,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 30,
+              right: 0,
+            ),
+            // 11
+            Positioned(
+              child: Text(
+                chef,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 10,
+              right: 0,
+            )
+          ],
+        ),
+
         // 1
         padding: const EdgeInsets.all(16),
         // 2
